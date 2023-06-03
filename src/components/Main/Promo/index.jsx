eda1d5a8c3876e69e311befd1ccaab229a9bdb85
Dmitry Alekseev <Ezzy-Way@yandex.ru>
2023-06-03 23:58:49 +0400

    added: styles Promo and NavTab

--- src/components/Main/Promo/index.jsx
+++ src/components/Main/Promo/index.jsx
@@ -1,14 +1,16 @@
 import webimg from '../../../images/Promo/webimage.png';
 
+import './Promo.css';
+
 function Promo () {
   return(
     <div className='promo'>
       <div className='promo__container'>
-        <h1 className='promo__header'>Учебный проект студента факультета Веб-разработки.</h1>
+        <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
         <p className='promo__text'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
         <button className='promo__button'>Узнать больше</button>
       </div>
-      <img className='promo__button' src={webimg} alt="Картинка Веб-разработки" />
+      <img className='promo__image' src={webimg} alt="Картинка Веб-разработки" />
     </div>
   )
 }
--- src/components/NavTab/index.jsx
+++ src/components/NavTab/index.jsx
@@ -1,12 +1,14 @@
 import logo from '../../images/logo.svg';
 
+import './NavTab.css';
+
 function NavTab () {
   return(
     <header className="header">
       <img className="header__logo" src={logo} height={38} width={38} alt="Логотип" />
       <div className="header__container">
         <a className="header__link" href=" ">Регистрация</a>
-        <a className="header__link" href=" ">Войти</a>
+        <a className="header__link header__button-link" href=" ">Войти</a>
       </div>
     </header>
   )
